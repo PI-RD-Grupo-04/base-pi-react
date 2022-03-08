a00b8c1ddf571c6f1516ae26045ed3fff3935ae2
washington <washingtondejesuspereira@hotmail.com>
2022-03-08 11:45:52 -0300

    washington

---
+++ src/pages/favorites/Favorites.js
@@ -0,0 +1,22 @@
+import './Favorites.css'
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer'
+
+
+
+
+function Favorites() {
+
+    return (
+        <>
+        <Header/>
+        
+        <Footer/>
+
+        </>
+    )
+
+
+}
+
+            export default Favorites;
