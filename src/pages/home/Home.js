bf0c2061314cb66149712d20b2fc5e96f1815d61
jefferson <guinho.psp1@gmail.com>
2022-03-08 16:13:55 -0300

    product-card

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,16 @@
 import './Home.css'
 import Header from '../../components/header/Header'
-import Footer from '../../components/footer/Footer'
+import Footer from '../../components/footer/Footer' 
+import ProductCard from '../../components/productCard/ProductCard'
 
 function Home() {
     return (
         <> 
         <Header/> 
         
-            <h1>Home</h1>
+            <h1>Produtos</h1>
+            <ProductCard link='https://i.ibb.co/nP8cZL2/abacaxi.png' nome="abacaxi" 
+            peso={1.00} price={12.90} />
             <Footer/>
         </>
     )
