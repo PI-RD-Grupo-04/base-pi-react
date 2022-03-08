a1901e9e6116fb1567047a8833700ea500b8317b
jefferson <guinho.psp1@gmail.com>
2022-03-08 15:40:25 -0300

    checkout feita

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,17 @@
 import { Switch, Route } from "react-router-dom";
 import Home from './pages/home/Home'
 import Product from './pages/product/Product'
-import Cart from './pages/cart/Cart'
+import Cart from './pages/cart/Cart' 
+import Checkout from './pages/checkout/Checkout'
 
 export const Routes = () => {
     return (
         <Switch>
             <Route path="/" component={Home} exact/>
             <Route path="/product" component={Product} />
-            <Route path="/cart" component={Cart} />
+            <Route path="/cart" component={Cart} /> 
+            <Route path="/checkout" component={Checkout} /> 
+
         </Switch>
     )
 }
