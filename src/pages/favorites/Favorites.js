45bd80bc84e70b5211256ad30676d836a8a1aa5d
washington <washingtondejesuspereira@hotmail.com>
2022-03-08 12:11:02 -0300

    subindo conteudo main

---
+++ src/pages/favorites/Favorites.js
@@ -0,0 +1,36 @@
+import './Favorites.css'
+import Header from '../../components/header/Header'
+import Footer from '../../components/footer/Footer'
+import CustomerMenu from '../../components/customerMenu/CustomerMenu'
+
+
+
+function Favorites() {
+
+    return (
+        <>
+            <Header />
+            <CustomerMenu />
+            <div class="col-12 col-md-9 order-md-last  mb-3">
+                <h3 class="text-center title-main"> Meus Favoritos </h3>
+                {/* inicio produtos */}
+                <div class="row">
+                    {/* ********************* item unidade  *********************/}
+                    <div class="col-12 col-sm-6 col-lg-4">
+                        
+
+
+                     </div>
+                </div>
+            </div>
+
+
+                        <Footer />
+
+                    </>
+                    )
+
+
+}
+
+                    export default Favorites;
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router-dom";
 import Home from './pages/home/Home'
 import Product from './pages/product/Product'
 import Cart from './pages/cart/Cart'
+import Favorites from "./pages/favorites/Favorites";
 
 export const Routes = () => {
     return (
@@ -9,6 +10,7 @@ export const Routes = () => {
             <Route path="/" component={Home} exact/>
             <Route path="/product" component={Product} />
             <Route path="/cart" component={Cart} />
+            <Route path="/favorites" component={Favorites} />
         </Switch>
     )
 }
